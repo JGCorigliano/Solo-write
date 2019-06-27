f981bd6288e9c6f9b47b0424a62baec5b34126d9
JGCorigliano <juan.g.corigliano@gmail.com>
2019-06-27 05:14:11 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const texts = ["websites","ilustraciones","milanesas", "boludeces","floreros"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+if(count === texts.length){
+    count = 0;
+}
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    
+    document.querySelector(".escribir").textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 300);
+
+})();
